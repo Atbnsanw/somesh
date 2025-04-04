7315b165737da6584b091492580e245c2f170fa6
Atbnsanw <37361106+Atbnsanw@users.noreply.github.com>
2025-04-05 05:44:25 +0800

    Update thfhe.rs

--- thfhe.rs
+++ thfhe.rs
@@ -19,14 +19,14 @@ struct Args {
     n: u32,
     // 参数 t
     #[arg(short = 'i')]
-    i: u32,
+    id: u32,
 }
 
 fn main() {
     let args = Args::parse();
     //const NUM_PARTIES: u32 =args.n;
     let number_parties = args.n;
-    let party_id = args.i;
+    let party_id = args.id;
     //let number_threshold = args.t;
     let number_threshold = (number_parties - 1) / 2;
     //const THRESHOLD: u32 = args.t;
@@ -114,25 +114,24 @@ fn thfhe(party_id: u32, num_parties: u32, threshold: u32, base_port: u32) {
     
 
     //---------------------------
+    let test_num = 20000;
+    let public_a = vec![public_a_t.clone();test_num];
+    let public_b = vec![public_b_t;test_num];
     backend.init_z2k_triples_from_files();
     if party_id <= threshold {
         
-        let test_num = 10000;
-        let public_a = vec![public_a_t.clone();test_num];
-        let public_b = vec![public_b_t;test_num];
-        
+
         let (my_dd_res, (online_duration, offline_duration)) =
             distdec(&mut backend, rng, &public_a, &public_b, &my_sk);
-    
         println!(
             "Party {} had finished the dd-online with time {} ns,",
             party_id,
-            (online_duration+online_duration).as_nanos()
+            online_duration.as_nanos()
         );
         println!(
             "Party {} had finished the dd-offline with time {} ns,",
             party_id,
-            (offline_duration+offline_duration).as_nanos()
+            offline_duration.as_nanos()
         );
 
         if party_id == 0 {
